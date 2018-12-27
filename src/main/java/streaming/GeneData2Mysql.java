1fcf2e617522dedb73550ef69745e0222b3f68af
suifengzhuliu <zhangjunemail@126.com>
2018-12-27 14:03:04 +0800

    aaaaaa

--- src/main/java/streaming/GeneData2Mysql.java
+++ src/main/java/streaming/GeneData2Mysql.java
@@ -6,11 +6,9 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010;
 
 import streaming.mysqlsink.MysqlSink;
 
@@ -24,43 +22,7 @@ public class GeneData2Mysql {
 	public static void main(String[] args) throws Exception {
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		final String province[] = new String[] { "上海", "云南", "内蒙", "北京", "吉林", "四川", "国外", "天津", "宁夏", "安徽", "山东", "山西",
-				"广东", "广西", "江苏", "江西", "河北", "河南", "浙江", "海南", "湖北", "湖南", "甘肃", "福建", "贵州", "辽宁", "重庆", "陕西", "香港",
-				"黑龙江" };
-
-		DataStream<List> source = env.addSource(new SourceFunction<List>() {
-
-			private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-			private volatile boolean isRunning = true;
-			private static final long serialVersionUID = 1L;
-			long count = 0;
-			private Date date;
-			private AtomicLong al = new AtomicLong(0L);
-
-			@Override
-			public void run(SourceContext<List> ctx) throws Exception {
-				while (isRunning) {
-					Thread.sleep(10);
-					// 省市、id、datestamp、date、计数,
-					List list = new ArrayList();
-					date = new Date();
-					StringBuffer ss = new StringBuffer();
-					String pro = province[(int) (Math.random() * 29)];
-					list.add(pro);
-					String id = "id-" + (int) (Math.random() * 1000);
-					list.add(id);
-					list.add(date.getTime());
-					list.add(sdf.format(date));
-					list.add(al.incrementAndGet());
-					ctx.collect(list);
-				}
-			}
-
-			@Override
-			public void cancel() {
-				isRunning = false;
-			}
-		});
+		DataStream<List> source = env.addSource(new GeneUISource());
 
 //		source.print();
 
@@ -70,4 +32,6 @@ public class GeneData2Mysql {
 		source.addSink(sink);
 		env.execute("gene data to mysql");
 	}
+
+
 }
